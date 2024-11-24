19fcf346093f2cb214135911e9802d7a9c6eacd1
Alex <alikrieger1@gmail.com>
2024-11-24 13:28:23 +0100

    update: moved style to style.css

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -11,7 +11,7 @@ function Home2() {
       <Container>
         <Row>
               <Col md={8} className="home-about-description">
-                <h1 style={{ fontSize: "2.6em" }}>
+                <h1 style="intro-heading">
                   LET ME <span className="blue"> INTRODUCE </span> MYSELF
                 </h1>
                 <p className="home-about-body">
